572faaf63e985f59a91dc5c2f2b280a562d66aa8
Cheng <Cheng.Han@harmn.com>
2024-12-24 15:57:59 +0800

    解决合并冲突

--- test_1.c
+++ test_1.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
-    printf("hello world! --- fitst");
-    return 0;
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* stableMountains(int* height, int heightSize, int threshold, int* returnSize) {
+    int *answer = (int *)malloc(sizeof(int) * (heightSize - 1));
+    int i = 0, j = 0;
+    for(i = 1; i <= heightSize - 1; i++){
+        if(height[i-1] > threshold){
+            answer[j++] = i;
+        }
+    }
+    *returnSize = j;
+    return answer;
 }
