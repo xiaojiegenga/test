af78be6ce0ac74585556fd408eab81220d596013
Cheng <Cheng.Han@harmn.com>
2025-01-07 10:51:11 +0800

    test_1.c verify

--- test_1.c
+++ test_1.c
@@ -1,19 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int tailRecur(int n, int res) 
-// "尾递归求n个数的和"
+typedef struct ListNode
 {
-    if(n == 0){
-        return res;
-    }
-    return tailRecur(n-1,res+n);
+    int val;
+    struct ListNode* next;
+}ListNode;
+
+ListNode* newListNode(int val)
+{
+    ListNode* node;
+    node = (ListNode*)malloc(sizeof(ListNode));
+    node->val = val;
+    node->next = NULL;
+    return node;
 }
 
-int main() 
+void insert(ListNode* n0, ListNode* p)
 {
-    int n = 10;
-    int res = tailRecur(n,0);
-    printf("The sum of first %d numbers is %d\n",n,res);
+    ListNode* q = n0->next;
+    p->next = q;
+    n0->next = p;
+}
+
+void removeOneItem(ListNode* n0)
+{
+    if(!n0->next)
+    {
+        return;
+    }
+    //n0->q->n1
+    ListNode* q = n0->next;
+    n0->next = q->next;
+    free(q);
+}
+
+ListNode* access(ListNode* head, int index)
+{
+    ListNode* p = head;
+    for(int i = 0; i < index; i++)
+    {
+        if(head == NULL)
+            return NULL;
+        p = p->next;
+    }
+    return p;
+}
+
+int findListNode(ListNode* head, int val)
+{
+    ListNode* p = head;
+    int index = 0;
+    while(p)
+    {
+        if(p->val == val)
+            return index;
+        p = p->next;
+        index++; 
+    }
+    return -1;
+}
+
+int main(void)
+{
+    ListNode* head = newListNode(1);
+
     return 0;
 }
